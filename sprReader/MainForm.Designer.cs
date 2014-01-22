255497f316b6fdcf6f88151cdd91ecb076931b78
igi <bt_igi@outlook.com>
2014-01-22 12:37:51 +0000

    Initial commit

---
+++ sprReader/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace sprReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.rtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(12, 12);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtOutput
+            // 
+            this.rtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtOutput.Location = new System.Drawing.Point(12, 52);
+            this.rtOutput.Name = "rtOutput";
+            this.rtOutput.Size = new System.Drawing.Size(665, 263);
+            this.rtOutput.TabIndex = 1;
+            this.rtOutput.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 336);
+            this.Controls.Add(this.rtOutput);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "MainForm";
+            this.Text = "SprReader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.RichTextBox rtOutput;
+    }
+}
+
---
+++ sprReader/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace sprReader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var converter = new sprConverter();
+            try
+            {
+                converter.Palette = Image.FromFile(@"D:\tmp\Dark Reign\def_pal.bmp") as Bitmap;
+                converter.Open(@"D:\tmp\Dark Reign\ecsmosm1.spr");
+                //converter.Open(@"D:\tmp\Dark Reign\uowtrst0.spr");
+                //converter.Open(@"D:\tmp\Dark Reign\aoctr007.spr");
+                //converter.Open(@"D:\tmp\Dark Reign\aoclf000.spr");
+                converter.Parse();
+
+                //var sprFiles = new List<string>(Directory.EnumerateFiles(@"D:\tmp\Dark Reign\", "*.spr", SearchOption.AllDirectories));
+                //foreach (var sprFile in sprFiles)
+                //{
+                //    converter.Open(sprFile);
+                //    converter.Parse();
+                //}
+            }
+            catch (Exception ex)
+            {
+                rtOutput.Text += System.Environment.NewLine + converter.filename;
+                System.Diagnostics.Trace.Write(ex.ToString());
+            }
+            finally
+            {
+                converter.Close();
+            }
+        }
+    }
+}
